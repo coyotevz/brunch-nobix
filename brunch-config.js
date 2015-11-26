62b02ce74b8fc712195860c449a49ccb255f54f6
Augusto Roccasalva <augusto@rioplomo.com.ar>
2015-11-26 03:52:49 -0300

    Add brunch-config and index.html

---
+++ brunch-config.js
@@ -0,0 +1,35 @@
+exports.config = {
+  files: {
+    javascripts: {
+      joinTo: {
+        'js/app.js': /^app/,
+        'js/vendor.js': /^(?!app)/
+      }
+    },
+    stylesheets: {
+      joinTo: 'css/app.css'
+    },
+    templates: {
+      joinTo: 'js/app.js'
+    }
+  },
+
+  plugins: {
+    jshint: {
+      pattern: /^app\/.*\.js$/,
+      options: {
+        bitwise: true,
+        curly: true,
+        undef: true,
+        unused: true,
+        strict: "global",
+      },
+      globals: {
+        jQuery: true,
+        console: true,
+        //module: true,
+      },
+      warnOnly: true
+    }
+  }
+};

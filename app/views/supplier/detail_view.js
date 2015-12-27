cab77954a3ec7d5dff851af9f65098d18a537258
Augusto Roccasalva <augusto@rioplomo.com.ar>
2015-12-27 06:09:19 -0300

    Better edit trigger

--- app/views/supplier/detail_view.js
+++ app/views/supplier/detail_view.js
@@ -4,8 +4,18 @@ var SupplierDetailView = View.extend({
   autoRender: false,
   template: 'supplier/detail.html',
 
+  initialize: function () {
+    View.prototype.initialize.apply(this, arguments);
+    this.delegate('click', '.action-edit', this.edit);
+  },
+
   listen: {
     'sync model': 'render',
+  },
+
+  edit: function(evt) {
+    evt.stopPropagation();
+    console.log('#edit:', arguments);
   }
 });
 
